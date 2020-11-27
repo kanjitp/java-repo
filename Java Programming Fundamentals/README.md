5b665d05e8e06c840419dc0ed2234682d29477c0
Kan Jitpakdi <mavmerlin@mvmln-mac.local>
2020-11-27 21:39:16 +0800

    add README and reverse reverse euclidean algorithm

---
+++ Java Programming Fundamentals/README.md
@@ -0,0 +1,4 @@
+# Java Programming Fundamental
+
+## Keywords
+1. The source for folders starting with tutorial is mostly from [University of Helsinki MOOC](java-programming.mooc.fi)
--- Java Programming Fundamentals/tutorial2/euclideanAlgorithm.java
+++ Java Programming Fundamentals/tutorial2/euclideanAlgorithm.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class euclideanAlgorithm {
     public static void main(String[] args) {
-        gcdExtended(12327, 2409);
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("\na: ");
+        int a = sc.nextInt();
+        System.out.printf("\nb: ");
+        int b = sc.nextInt();
+        System.out.println();
+        gcdExtended(a, b);
+        bezoutLemma(a, b);
     }
 
     public static int gcd(int a, int b) {
@@ -16,7 +24,6 @@ public class euclideanAlgorithm {
             b = temp;
         }
 
-
         int r = b;
 
         System.out.println("-- Applying Euclidean Algorithm");
@@ -28,8 +35,29 @@ public class euclideanAlgorithm {
             b = r;
         }
 
+    }
+    public static void bezoutLemma(int a, int b) {
+        System.out.println("-- Applying Reverse Euclidean Algorithm");
+        int r1 = a;
+        int r2 = b;
+        int u1 = 1;
+        int v1 = 0;
+        int u2 = 0;
+        int v2 = 1;
 
+        while (r2 != 0) {
+            int quotient = (int) r1 / r2;
+            int rs = r1;
+            int us = u1;
+            int vs = v1;
+            r1 = r2;
+            u1 = u2;
+            v1 = v2;
+            r2 = rs - quotient * r2;
+            u2 = us - quotient * u2;
+            v2 = vs - quotient * v2;
+        }
+        System.out.printf("%6d = %6d x %6d + %6d x %6d ", r1, u1, a, v1, b);
 
-        
     }
 }
