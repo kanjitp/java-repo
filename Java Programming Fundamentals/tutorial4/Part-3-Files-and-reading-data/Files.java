f955bd9ebec2297dce2be98c4a691056fa384337
Kan Jitpakdi <mavmerlin@mvmln-mac.local>
2020-11-29 00:58:46 +0800

    fix Files.java

--- Java Programming Fundamentals/tutorial4/Part-3-Files-and-reading-data/Files.java
+++ Java Programming Fundamentals/tutorial4/Part-3-Files-and-reading-data/Files.java
@@ -1,13 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
 import java.nio.file.Paths;
 
 public class Files {
-    public static void main(String[] args) {
+    public static void main(String[] args) {   
         try {
             Scanner sc = new Scanner(Paths.get("plist.xml"));
             while (sc.hasNextLine()) {
                 String row = sc.nextLine();
-                System.out.println(row);
+                if(row.contains("<Row>")) {
+                    ArrayList<String> elements = new ArrayList<>();
+                    String nextRow = sc.nextLine();
+                    while (!nextRow.contains("</Row>")) {
+                        elements.add(nextRow);
+                        nextRow = sc.nextLine();
+                    }
+                    for (String element : elements) {
+                        element = element.replaceAll(" <Cell><Data ss:Type=\"String\">", "");
+                        element = element.replaceAll(" <Cell><Data ss:Type=\"Number\">", "");
+                        element = element.replaceAll("</Data></Cell>", ""); 
+                        System.out.printf(element + " ");
+                    }
+                    System.out.println();
+                }
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
